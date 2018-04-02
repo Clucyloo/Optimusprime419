8df02f8a3dbacb2d7346680e03d85c4cab7bb9e6
Clucyloo <38012667+Clucyloo@users.noreply.github.com>
2018-04-02 13:07:42 -0400

    Add files via upload

---
+++ Phonebook/src/NewPerson.java
@@ -0,0 +1,16 @@
+public class NewPerson {
+
+int num = 0;
+String name = "", note = "";
+
+NewPerson(){
+    int num = number;
+
+
+}
+public void Addperson (int num, String name, String note){
+    num =
+
+
+}
+}
---
+++ Phonebook/src/Phonebook/Phonebook.java
@@ -0,0 +1,12 @@
+package Phonebook;
+
+import java.util.Scanner;
+
+public class Phonebook {
+public static void main(String[] args){
+Scanner input = new Scanner(System.in);
+int number = 0;
+NewPerson john = new
+
+}
+}
